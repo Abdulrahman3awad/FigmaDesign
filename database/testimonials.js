e67576520e79e2ec776758f430252826be898e35
Abdulrahman3awad <abdh.3wad@gmail.com>
2023-05-01 14:02:44 +0200

    starting the  project

---
+++ database/testimonials.js
@@ -0,0 +1,23 @@
+const testimonials = [
+    {
+        "username": "Jacob Jones", "subtitle": "Student, National University",
+        "feedback": "Courses was fantastic! It is Master platform for those looking to start a new career, or need a refresher."
+    },
+    {
+        "username": "Jacob Jones", "subtitle": "Student, National University",
+        "feedback": "Courses was fantastic! It is Master platform for those looking to start a new career, or need a refresher."
+    },
+    {
+        "username": "Jacob Jones", "subtitle": "Student, National University",
+        "feedback": "Courses was fantastic! It is Master platform for those looking to start a new career, or need a refresher."
+    },
+    {
+        "username": "Jacob Jones", "subtitle": "Student, National University",
+        "feedback": "Courses was fantastic! It is Master platform for those looking to start a new career, or need a refresher."
+    },
+    {
+        "username": "Jacob Jones", "subtitle": "Student, National University",
+        "feedback": "Courses was fantastic! It is Master platform for those looking to start a new career, or need a refresher."
+    }
+]
+export default testimonials
